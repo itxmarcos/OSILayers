0bc9ca731f7be02bf9a99438648fb3e4556ffc15
Marcos Caballero <m.caballeroyus@gmail.com>
2019-11-11 17:47:05 +0100

    fixedall

--- src/ARP.java
+++ src/ARP.java
@@ -3,11 +3,6 @@ import java.util.Map;
 
 
 public class ARP extends Protocol{
-<<<<<<< HEAD
-    //Map<String, Integer> numberMapping = new HashMap<>(); //Esto se encargarÃ¡ de traducir de IP a MAC
-	//AquÃ­ tiene que printear paquetes
-=======
-    //Map<String, Integer> numberMapping = new HashMap<>(); //Esto se encargará de traducir de IP a MAC
-	//Aquí tiene que printear paquetes
->>>>>>> c2b8354376720865637ea2cba2b20646ff1c9e9e
+    //Map<String, Integer> numberMapping = new HashMap<>(); //Esto se encargarï¿½ de traducir de IP a MAC
+	//Aqui tiene que printear paquetes
 }
--- src/Ipv4.java
+++ src/Ipv4.java
@@ -1,8 +1,4 @@
 
 public class Ipv4 extends Protocol {
-<<<<<<< HEAD
-	//AquÃ­ tiene que printear paquetes
-=======
 	//Aquí tiene que printear paquetes
->>>>>>> c2b8354376720865637ea2cba2b20646ff1c9e9e
 }
